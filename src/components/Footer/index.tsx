43d9df19b06794ba13edac5d886f8160d6869b85
hanyaojie <hanyaojie@bytedance.com>
2023-06-23 22:17:08 +0800

    chore: 文本修改

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -6,7 +6,7 @@ const Footer: React.FC = () => {
 		<div className={styles.Footer}>
 			<ul className="flexClmCenter" style={{ lineHeight: '25px' }}>
 				<li>©2023 - 2024 By HHU_软开课设_秦岭组</li>
-				<li>团队成员：韩耀杰、李雅慧、TC、JX</li>
+				<li>团队成员：韩耀杰、李雅慧、葛天辰、李嘉轩</li>
 				<li>欢迎体验我们的电影推荐系统！</li>
 			</ul>
 		</div>
